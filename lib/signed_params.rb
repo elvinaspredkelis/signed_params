0daffb7b78288f5ca165749253251d4ac0024bcf
Kasper Timm Hansen <hey@kaspth.com>
2025-05-30 19:20:08 +0200

    Expose `params.signed` and `params.sign`
    When finding a signed param:
    
    ```ruby
    @post = Post.find params.signed[:id]
    @post = Post.find params.signed.fetch(:id) # can raise ActionController::Parameters::InvalidSignature
    ```
    
    And you can embed signed parameters in URLs like this:
    
    ```ruby
    post_url params.sign(id: @post.id)
    ```

--- lib/signed_params.rb
+++ lib/signed_params.rb
@@ -1,15 +1,29 @@
-require "signed_params/concern"
-require "signed_params/configuration"
 require "signed_params/version"
+require "action_controller/metal/strong_parameters"
 
-module SignedParams
-  class << self
-    def configuration
-      @configuration ||= Configuration.new
-    end
+class ActionController::Parameters::Signed < Data.define(:verifier, :params)
+  ActionController::Parameters::InvalidSignature = Class.new StandardError
 
-    def configure
-      yield configuration
-    end
+  def [](key)
+    verifier.verified(params[key])
+  end
+
+  def fetch(key)
+    verifier.verify(params[key])
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    raise ActionController::Parameters::InvalidSignature
   end
 end
+
+module ActionController::Parameters::Signed::Integration
+  def self.included(parameters)
+    parameters.mattr_accessor :verifier
+  end
+
+  def sign(**params)
+    params.transform_values { verifier.generate _1 }
+  end
+  def signed = @signed ||= ActionController::Parameters::Signed.new(verifier, self)
+end
+
+require_relative "signed_params/railtie" if defined?(Rails::Railtie)
